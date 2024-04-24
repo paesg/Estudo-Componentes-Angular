5ea60d57066b9217aba271e8a12614be86397709
Gabriel Paes <gabrielpaes99@gmail.com>
2024-04-24 20:49:49 -0300

    Estudos Angular - Componentes

---
+++ src/app/cards/cards.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
+import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    CardButtonComponent,
+    CardRoxoComponent,
+    CardRoxoButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CardComponent,
+    // CardButtonComponent,
+    CardRoxoComponent,
+    // CardRoxoButtonComponent -- Não preciso declarar caso não seja diretamente declarado no outro modulo.
+  ]
+})
+export class CardsModule { }
